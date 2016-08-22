344c0e2c8104f3ffabc0c005a2c059efd321b50d
Mark Menard <mark.joseph.menard@gmail.com>
2016-08-22 13:46:46 -0500

    Visual Studios
    worked on loops and conditioal statements

--- Condtional_Statements/Condtional_Statements/Source.cpp
+++ Condtional_Statements/Condtional_Statements/Source.cpp
@@ -6,176 +6,322 @@ int main()
 
 	////question 1
 
-	//int y = 0;
-	//if (y == 0)
-	//{
-	//	printf("x = 100");
+	////int y = 0;
+	////if (y == 0)
+	////{
+	////	printf("x = 100");
 
 
-	//}
-	//system("pause");
+	////}
+	////system("pause");
 
 
 
-	//int z = 55;
-	//int t = 10;
+	////int z = 55;
+	////int t = 10;
 	////question 2
 
 
-	//if (z > t)
+	////if (z > t)
+	////{
+	////	printf("%i", z);
+
+	////}
+	////else
+	////{
+	////	printf("%i", t);
+
+	////}
+
+	/// Question 3
+
+	//int num1 = 12;
+	//int num2 = 4;
+	//int num3 = 21;
+	//int order1;
+	//int order2;
+
+	//// case 1
+	//if (num1 > num2 && num1 > num3);
 	//{
-	//	printf("%i", z);
-
+	//	if (num2 > num3);
 	//}
-	//else
+	//	
+	//
+	////case 2
+	//if (num2 < num1 && num1 < num3);
 	//{
-	//	printf("%i", t);
-
+	//	if (num1 < num3);
 	//}
-
+	//
+	////case 3
+	//if (num3 > num1 && num2 > num1)
+	//{
+	//	if (num1 > num2);
+	//}
 	//system("pause");
+
+
+
+
+
+
+
+
+
+
+
 
 	////Question 4
 
-	//int choice;
-	//std::cin >> choice;
-	//switch (choice)
-	//{
-	//case 1:
-	//	std::cout << "1";
-	//	break;
-	//case 2:
-	//	std::cout << "2 or 3";
-	//	break;
-	//case 3:
-	//	std::cout << "2 or 3";
-	//	break;
-	//case 4:
-	//	std::cout << "4";
-	//	break;
-	//default:
-	//	std::cout << "Invaild";
-	//	return 0;
+	////int choice;
+	////std::cin >> choice;
+	////switch (choice)
+	////{
+	////case 1:
+	////	std::cout << "1";
+	////	break;
+	////case 2:
+	////	std::cout << "2 or 3";
+	////	break;
+	////case 3:
+	////	std::cout << "2 or 3";
+	////	break;
+	////case 4:
+	////	std::cout << "4";
+	////	break;
+	////default:
+	////	std::cout << "Invaild";
+	////	return 0;
 
 
-	//}
+	////}
 
 
-	//system("pause");
+	////system("pause");
 
 	////question 5
-	//int x = 0;
-	//y = 10;
-	//int result = 0;
-	//if (x > y)
-	//{
-	//	result = 0;
-	//}
+	////int x = 0;
+	////y = 10;
+	////int result = 0;
+	////if (x > y)
+	////{
+	////	result = 0;
+	////}
 
-	//else
-	//{
-	//	result = 10;
+	////else
+	////{
+	////	result = 10;
 
-	//	int biggerNum = (x > y) ? x : y;
-	//	std::cout << biggerNum;
+	////	int biggerNum = (x > y) ? x : y;
+	////	std::cout << biggerNum;
 
-	//	system("pause");
+	////	system("pause");
 
 
-	//}
+	////}
 
 	////question 6
 
-	//int g = 5;
-	//int	h = 10;
-	//int c = 15;
+	////int g = 5;
+	////int	h = 10;
+	////int c = 15;
 
-	//if (g + h == c)
-	//	std::cout << "c";
+	////if (g + h == c)
+	////	std::cout << "c";
 
-	//system("pause");
+	////system("pause");
 
 	////question 7
 
-	//int month;
-	//std::cin >> month;
-	//switch (month)
+	////int month;
+	////std::cin >> month;
+	////switch (month)
+	////{
+	////case 1:
+	////	std::cout << "January has 31 days";
+	////	break;
+	////case 2:
+	////	std::cout << "February has 29 days";
+	////	break;
+	////case 3:
+	////	std::cout << "March has 31 days";
+	////	break;
+	////case 4:
+	////	std::cout << "April has 30 days";
+	////	break;
+	////case 5:
+	////	std::cout << "May has 31 days";
+	////	break;
+	////case 6:
+	////	std::cout << "June has 30 days";
+	////	break;
+	////case 7:
+	////	std::cout << "July has 31 days";
+	////case 8:
+	////	std::cout << "August has 31 days";
+	////	break;
+	////case 9:
+	////	std::cout << "September has 30 days";
+	////case 10:
+	////	std::cout << "October has 31 days";
+	////	break;
+	////case 11:
+	////	std::cout << "November has 30 days";
+	////	break;
+	////case 12:
+	////	std::cout << "December has 31 days";
+	////	break;
+
+
+	////}
+
+	////system("pause");
+
+	////for (int i = 0; i <= 25; i++)
+	////{
+	////	std::cout << i;
+	////}
+
+	//// 8. Conditionals 
+	////	 a. TRUE
+	////	/*int numPos = 35;
+	////	int numNeg = -55;
+	////	{
+	////		numPos > numNeg;
+	////	}
+	////	system("pause");*/
+
+	////	b. FALSE
+	////	/*char frstChar = 'j';
+	////	char scndChar = '0';
+
+	////	{
+	////		frstChar > scndChar;
+	////	}
+
+	////	system("pause");
+
+	////	 c. TRUE
+
+	////	int numPos = 35;
+	////	int numNeg = -55;
+	////	{
+	////		!(numPos + numNeg);
+	////	}
+	////	system("pause");
+
+	////	d.  TRUE
+
+	////	/*int numPos = 35;
+	////	int numNeg = -55;
+	////	{
+	////		(numPos == -30) || (numNeg == -55);
+	////	}
+	////	system("pause");*/
+
+	////	e.  FALSE
+	////	
+	////	/*double frstPrice = 4.1 && 9.9;
+	////	
+
+	////	{
+	////		(frstPrice >= 4.1) && (frstPrice <= 9.9);
+	////	}
+	////	system("pause");*/
+
+	////	f. TRUE
+	////	
+	////	bool flag = true;
+	////	char scndChar = 'r';
+	////	{
+	////		!flag && (scndChar <= 'r');
+
+	////	}
+	////	system("pause");
+
+	////	 g. TRUE
+	////	
+	////	/*bool flag = true;
+	////	int numPos = 66 && 35;
+	////	{
+	////		(numPos < 66) || (flag && numPos > 35);
+	////	}
+	////	system("pause");*/
+
+	////	 h. TRUE
+
+	////	int numPos = 36;
+	////	{
+	////		++numPos == 36;
+	////	}
+	////	system("pause");
+
+	////	i.  TRUE
+	////		
+	////	/*int numPos = 36;
+	////	{
+	////		numPos++ == 36;
+	////	}
+	////	system("pause");*/
+
+		////j.  false
+
+		//char frstchar = 'j' && 'J';
+
+
+		//{
+		//	(frstchar == "j") || (frstchar == 'J');
+		//}
+		//	
+		//	system("pause");
+
+
+
+		//9. 
+		// a. (true or false) || (true and false)
+		// b. not true  and true || (true and false)
+		// c. ( not 5 or true) || (not false ) and (not true and not false)
+		// d.  true or fasle  and true
+		// e. not true and false
+
+
+
+
+	//// extra practice for loops
+	////int health = 20;
+	////while (health > 0)
+	////{
+	////	health--;
+	////}
+
+
+	///*for (int w = 0; w < 100; w++)
 	//{
-	//case 1:
-	//	std::cout << "January has 31 days";
-	//	break;
-	//case 2:
-	//	std::cout << "February has 29 days";
-	//	break;
-	//case 3:
-	//	std::cout << "March has 31 days";
-	//	break;
-	//case 4:
-	//	std::cout << "April has 30 days";
-	//	break;
-	//case 5:
-	//	std::cout << "May has 31 days";
-	//	break;
-	//case 6:
-	//	std::cout << "June has 30 days";
-	//	break;
-	//case 7:
-	//	std::cout << "July has 31 days";
-	//case 8:
-	//	std::cout << "August has 31 days";
-	//	break;
-	//case 9:
-	//	std::cout << "September has 30 days";
-	//case 10:
-	//	std::cout << "October has 31 days";
-	//	break;
-	//case 11:
-	//	std::cout << "November has 30 days";
-	//	break;
-	//case 12:
-	//	std::cout << "December has 31 days";
-	//	break;
+	//	std::cout << w;
+	//}
+	//int water = 100;
+	//while (water > 0)
+	//{
+	//	water--;
+	//}
+	//	system("pause");*/
 
 
+	// //Exercise Loops
+	//for (int i = 1000; i > 0; i-=5)
+	//{
+	//	std::cout << i  << "FizzBuzz";
+	//}
+	//int i = 0;
+	//while (i > 0)
+	//{
+	//	i--;
 	//}
 
 	//system("pause");
 
-	//for (int i = 0; i <= 25; i++)
-	//{
-	//	std::cout << i;
-	//}
-
-	// extra practice for loops
-	int health = 20;
-	while (health > 0)
-	{
-		health--;
-	}
+	
 
 	
-	//for (int w = 0; w < 100; w++)
-	//{
-	//	std::cout << w;
-	//}
-	int water = 100;
-	while (water > 0)
-	{
-		water--;
-	}
-
-	// Exercise Loops
-	for (int i = 100; i > 0; i-=2)
-	{
-		std::cout << i  << " ";
-	}
-	int i = 0;
-	while (i > 0)
-	{
-		i--;
-	}
-
-	system("pause");
-
-
 }
